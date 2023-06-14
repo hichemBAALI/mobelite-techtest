0d746f67bdac1ae1d737c6c0701732a533550832
hichemBaaliYA <hichem.baali.pro@gmail.com>
2023-06-14 23:29:00 +0100

    Initiating DetailScreen

---
+++ app/screens/HomeStack/DetailScreen/DetailScreen.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react'
+import { View } from 'react-native'
+
+const DetailScreen: FC = ({ navigation }: any) => {
+  console.log({
+    navigation,
+  })
+  return (
+    <View>
+      <View />
+    </View>
+  )
+}
+
+export default DetailScreen
